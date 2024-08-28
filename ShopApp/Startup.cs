78ad0395fd52eef4f24b762d05882e00cc52fce8
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-28 14:02:26 +0300

    Добавьте файлы проекта.

---
+++ ShopApp/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+namespace Shop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public async void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env) {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            if (env.IsProduction()) {
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello world");
+                });
+            }
+         
+        } 
+    }
+}
