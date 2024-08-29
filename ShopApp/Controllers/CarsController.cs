b64e376572fc39147a308e9bb4629931888405b8
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-29 12:57:52 +0300

    Категории товаров

--- ShopApp/Controllers/CarsController.cs
+++ ShopApp/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShopApp.Data.Interfaces;
+using ShopApp.Data.Models;
 using ShopApp.ViewModels;
 
 namespace ShopApp.Controllers
@@ -15,14 +16,42 @@ namespace ShopApp.Controllers
             _allCategories = iCarsCategory;
         }
 
-
-        public ViewResult List()
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")]
+        public ViewResult List(string category)
         {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.id);
+            }
+            else
+            {
+                if(string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Электромобили")).OrderBy(i => i.id);
+                    currCategory = "Электромобили";
+                }
+                else
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Классические")).OrderBy(i => i.id);
+                    currCategory = "Классические автомобили";
+                }
+
+               
+
+            
+            }
+            var carObj = new CarsListViewModel
+            {
+                allCars = cars,
+                currCategory = currCategory
+            };
             ViewBag.Title = "Страница с автомобилями";
-            CarsListViewModel obj = new CarsListViewModel();
-            obj.allCars = _allCars.Cars;
-            obj.currCategory = "Автомобили";
-            return View(obj);
+            
+            return View(carObj);
         }
     }
 }
